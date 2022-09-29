3dd2e3c7c0c7e23590ac02b806e1955ae5eba244
João Melo <jopcmelo@gmail.com>
2022-09-29 14:48:13 -0300

    feat: add query and mutation

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,9 +1,21 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input";
+import { AppointmentModel } from "../dtos/models/appointment-model";
 
 @Resolver()
 export class AppointmentsResolver {
-  @Query(() => String)
-  async helloWorld() {
-    return 'Hello, world!'
+  @Query(() => [AppointmentModel])
+  async appointments() {
+    return []
+  }
+
+  @Mutation(() => AppointmentModel)
+  async createAppointment(@Arg('data') data: CreateAppointmentInput) {
+    const appointment = {
+      startsAt: data.startsAt,
+      endsAt: data.endsAt
+    }
+
+    return appointment
   }
 }
