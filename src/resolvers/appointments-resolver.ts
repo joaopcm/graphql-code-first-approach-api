4cb7fae913bbeacbd72a9c130dbc7cccf9852640
João Melo <jopcmelo@gmail.com>
2022-09-29 14:52:58 -0300

    feat: add relationship between models

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,12 +1,18 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, FieldResolver, Mutation, Query, Resolver, Root } from "type-graphql";
 import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input";
 import { AppointmentModel } from "../dtos/models/appointment-model";
+import { CustomerModel } from "../dtos/models/customer-model";
 
-@Resolver()
+@Resolver(() => AppointmentModel)
 export class AppointmentsResolver {
   @Query(() => [AppointmentModel])
   async appointments() {
-    return []
+    return [
+      {
+        startsAt: new Date(),
+        endsAt: new Date(),
+      }
+    ]
   }
 
   @Mutation(() => AppointmentModel)
@@ -17,5 +23,14 @@ export class AppointmentsResolver {
     }
 
     return appointment
+  }
+
+  @FieldResolver(() => CustomerModel)
+  async customer(@Root() appointment: AppointmentModel) {
+    console.log(appointment)
+
+    return {
+      name: 'John Doe'
+    }
   }
 }
